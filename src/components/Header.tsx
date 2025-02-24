07ac04c0be0ba6db6c0fec1f064bfe7218be7314
Alex <alexboichev46@gmail.com>
2025-02-24 18:18:45 +0200

    add bag. Add style for bag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import {FaBasketShopping} from "react-icons/fa6";
+
 
 function Header() {
+
+    let [cartOpen, setCartOpen] = React.useState(false);
+
+    function onClickHandler() {
+        setCartOpen(cartOpen =!cartOpen);
+    }
+    // @ts-ignore
     return (
         <header>
             <div>
@@ -9,7 +18,16 @@ function Header() {
                     <li>About us</li>
                     <li>Contacts</li>
                     <li>Cabinet</li>
+                    <li>{// @ts-ignore
+                        <FaBasketShopping onClick={onClickHandler}  className={`shop-cart-button ${cartOpen && 'active'}`}/>
+                    }</li>
                 </ul>
+
+                {
+                    cartOpen && (
+                        <div className={'shop-cart'}></div>
+                    )
+                }
             </div>
             <div className={'presentation'}></div>
         </header>
